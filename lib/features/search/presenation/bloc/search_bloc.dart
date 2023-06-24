a39cf46278744d59372e03fca23da6de4b5437d4
Abdurahmon727 <abdurahmon727.uz@gmail.com>
2023-06-24 17:15:21 +0500

    fix: add condition to bloc

--- lib/features/search/presenation/bloc/search_bloc.dart
+++ lib/features/search/presenation/bloc/search_bloc.dart
@@ -15,29 +15,31 @@ class SearchBloc extends Bloc<SearchEvent, SearchState>
     implements CurrentPageSavable {
   SearchBloc() : super(const _SearchState()) {
     on<_Search>((event, emit) async {
-      emit(state.copyWith(status: FormzStatus.submissionInProgress));
-      final SearchRepository repo = SearchRepositoryImpl();
-      final int page =
-          (state.query == event.query && state.maxPage > state.currentPage)
-              ? state.currentPage + 1
-              : 1;
-      final result = await repo.getSearchResults(event.query, page);
-      result.either((error) {
-        emit(state.copyWith(
-          status: FormzStatus.submissionFailure,
-          errorMessage: error.errorMessage,
-          currentCardIndex: 0,
-        ));
-      }, (data) {
-        emit(state.copyWith(
-          query: event.query,
-          status: FormzStatus.submissionSuccess,
-          resultModels: data.$1,
-          currentPage: page,
-          maxPage: data.$2,
-          currentCardIndex: 0,
-        ));
-      });
+      if (event.query.isNotEmpty) {
+        emit(state.copyWith(status: FormzStatus.submissionInProgress));
+        final SearchRepository repo = SearchRepositoryImpl();
+        final int page =
+            (state.query == event.query && state.maxPage > state.currentPage)
+                ? state.currentPage + 1
+                : 1;
+        final result = await repo.getSearchResults(event.query, page);
+        result.either((error) {
+          emit(state.copyWith(
+            status: FormzStatus.submissionFailure,
+            errorMessage: error.errorMessage,
+            currentCardIndex: 0,
+          ));
+        }, (data) {
+          emit(state.copyWith(
+            query: event.query,
+            status: FormzStatus.submissionSuccess,
+            resultModels: data.$1,
+            currentPage: page,
+            maxPage: data.$2,
+            currentCardIndex: 0,
+          ));
+        });
+      }
     });
 
     on<_ChangeCurrentPageIndex>((event, emit) {
